82f76763b63a501caaa53334ff834be8a3b5227d
Theocode12 <vchidubem9@gmail.com>
2025-04-08 17:52:24 +0100

    add: start of scheduler

--- backend/app/websockets/scheduler/scheduler.py
+++ backend/app/websockets/scheduler/scheduler.py
@@ -1,40 +1,62 @@
+from abc import ABC, abstractmethod
+from asyncio import Event
 from typing import Any, Callable, Coroutine
 from ..publisher.message_broker import MessageBroker
 
-class BaseScheduler:
+class BaseScheduler(ABC):
     def __init__(self, game_id, broker: MessageBroker):
         self.game_id = game_id
-        self.publisher = broker
+        self.broker = broker
 
     async def publish(self, channel, message):
         """Publish data to the game channel."""
-        return await self.publisher.publish(self.game_id, channel, message)
+        return await self.broker.publish(self.game_id, channel, message)
 
-    def subscribe(self, channel):
-        pass
+    async def run(self):
+        raise NotImplementedError
     
     async def cleanup(self):
         pass
 
 class ControlScheduler(BaseScheduler):
+    def __init__(self, game_id, broker):
+        super().__init__(game_id, broker)
+        self.event = Event()
+        self.speed = 1 # in secs
+        self.controls = {
+            'start': self.start,
+            'pause': self.pause,
+            'resume': self.resume,
+            'adjust_speed': self.adjust_speed
+        }
+
     async def start(self):
-        pass
+        self.event.set()
 
     async def pause(self):
-        pass
+        self.event.clear()
 
     async def resume(self):
-        pass
+        self.event.set()
 
     async def adjust_speed(self, new_speed):
-        pass
+        self.speed = new_speed
+
+    async def subscribe(self, channel):
+        async for message in self.broker.subscribe(self.game_id, channel):
+            if message.get('type') and (control := self.controls.get(message.get('type'))):
+                await control()
 
 
 class PvAIScheduler(ControlScheduler):
-    pass
+    def run():
+        pass
+    
 
 class PvPControlScheduler(ControlScheduler):
-    pass
+    def run():
+        pass
 
 class SpectatorScheduler(BaseScheduler):
-    pass
+    def run():
+        pass
