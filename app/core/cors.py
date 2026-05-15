a477e4a67c33030790a7ca8e8fe11fb311a1e4b9
Theocode12 <vchidubem9@gmail.com>
2026-05-15 14:47:21 +0100

    fix: cors

--- app/core/cors.py
+++ app/core/cors.py
@@ -1,10 +1,13 @@
 from fastapi.middleware.cors import CORSMiddleware
 
+from os import getenv
+
+frontend_url = getenv("SIM_FRONTEND_URL")
 
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:8080"],
+        allow_origins=[frontend_url],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
